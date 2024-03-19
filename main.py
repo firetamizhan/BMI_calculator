c2a67f0f187c6cf7033bbc5ad95b49af52ab2b0f
kasir <kasiramsureshm719@gmail.com>
2024-03-19 21:58:19 +0530

    main.py file was added

---
+++ main.py
@@ -0,0 +1,12 @@
+# create a basic bmi calculator python file
+
+weight = input()
+height = input()
+
+
+weight_as_int=int(weight)
+height_as_float=float(height)
+
+bmi = weight_as_int/height_as_float ** 2
+
+
